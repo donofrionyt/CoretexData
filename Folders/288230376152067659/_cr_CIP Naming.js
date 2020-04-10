22cba6e3dbff69d5886aa5e9e612474872492235
donofrionyt <dodono@gmail.com>
2020-04-10 12:04:35 -0400

    04-10-2020

--- Folders/288230376152067659/_cr_CIP Naming.js
+++ Folders/288230376152067659/_cr_CIP Naming.js
@@ -9,14 +9,26 @@ var formSide = $EventCrossStepProperty ('inkAreaName', 'Ink Area Left');
 var colorSeps = $Uppercase($ColorID());
 ///////////////////// Collect Data \\\\\\\\\\\\\\\\\\\\\;
 
+//var totalForms = $BookFormCount()
+
+//example page numbers for a inside DT = 8 and 9
+//example page numbers for a outside DT = 8 and 1
 
 
 ///////////////////// Data Conditions \\\\\\\\\\\\\\\\\\\\\;
-if (formSide === 'Ink Area Left') {
-//    formPGNum = $FormNumber();
-      formPGNum = $LowerImpositionPageNumber();
+var formEven = $LowerImpositionPageNumber();    //should be 8 inside DT or 1 outside DT
+
+if ((formEven%2)===0) {
+	var formOdd = $HigherImpositionPageNumberIncludeVirtuals();    //formEven is divisble by 2 so = 9 for inside DT
 } else {
-    formPGNum = $HigherImpositionPageNumberIncludeVirtuals();
+	var formEven = $HigherImpositionPageNumberIncludeVirtuals();   //formEven wasn't divisble by 2 so = 8 outside DT
+	var formOdd = $LowerImpositionPageNumber();    //1 outside DT
+}
+
+if (formSide === 'Ink Area Left') {
+      formPGNum = formOdd;     //9 or 1
+} else {
+    formPGNum = formEven;      //8
 }
 ///////////////////// Data Conditions \\\\\\\\\\\\\\\\\\\\\;
 
@@ -24,6 +36,7 @@ if (formSide === 'Ink Area Left') {
 
 ///////////////////// Data Results \\\\\\\\\\\\\\\\\\\\\;
 var cipName = pubDate + gossRunName + '_' + sektion + '_' + formPGNum + '_' + colorSeps + '.ppf';
+//var cipName = pubDate + gossRunName + '_' + sektion + '_' + formEven + '_' + formOdd + '_' + colorSeps + '.ppf';
 ///////////////////// Data Results \\\\\\\\\\\\\\\\\\\\\;
 
 cipName;
